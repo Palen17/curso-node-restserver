05c1dd98818d153eeae22203c4b236dcbf847726
valen napolitano <valentinnapo@gmail.com>
2024-02-15 12:26:21 -0300

    fin curso

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,19 +1,30 @@
 
-const {response} = require('express')
+const {response, request} = require('express')
 const Usuario = require('../models/usuario')
 const bcryptjs = require('bcryptjs')
 const {validationResult} = require("express-validator");
-const usuariosGet = (req, res = response) => {
+const usuariosGet = async (req= request, res = response) => {
 
-    const {q, nombre = 'No name', apikey, page = 1, limit}  = req.params
+    //const {q, nombre = 'No name', apikey, page = 1, limit}  = req.params
+
+    const {limite = 5, desde = 0} = req.query
+    const query = {estado: true}
+    //const usuarios = await Usuario.find(query)
+      // .skip(Number(desde))
+       // .limit(Number(limite))
+
+    //const total = await Usuario.countDocuments(query)
+
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ])
 
     res.json({
-        msg:'get api',
-        q,
-        nombre,
-        apikey,
-        page,
-        limit
+        total,
+        usuarios
     })
 }
 
@@ -69,11 +80,16 @@ const usuariosPatch = (req, res = response) => {
     })
 }
 
-const usuariosDelete = (req, res = response) => {
+const usuariosDelete = async (req, res = response) => {
 
-    res.json({
-        msg:'delete api'
-    })
+    const {id} = req.params
+
+    //Borrar fisicamente de la bd
+    //const usuario = await Usuario.findByIdAndDelete(id)
+
+    const usuario = await Usuario.findByIdAndUpdate(id, {estado: false})
+
+    res.json(usuario)
 }
 
 module.exports={
--- routes/user.js
+++ routes/user.js
@@ -31,7 +31,12 @@ router.post('/',[
     validarCampos
 ], usuariosPost)
 
-router.delete('/', usuariosDelete)
+router.delete('/:id', [
+        check('id', 'No es un id valido').isMongoId(),
+        check('id').custom(existeUsuarioPorId),
+    validarCampos
+    ],
+    usuariosDelete)
 
 
 module.exports = router
